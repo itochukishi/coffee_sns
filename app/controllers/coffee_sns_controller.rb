c9730c10d20cc3495cd388e1f0358ba251dce6ad
itochukishi <kishi-h@itochu.co.jp>
2019-02-23 19:50:46 +0900

    first commit

---
+++ app/controllers/coffee_sns_controller.rb
@@ -0,0 +1,21 @@
+class CoffeeSnsController < ApplicationController
+
+ def index
+ end
+
+ def create
+ end
+
+ def show
+ end
+
+ def destory
+ end
+
+ def new
+ end
+
+ def update
+ end
+
+end
---
+++ app/helpers/coffee_sns_helper.rb
@@ -0,0 +1,2 @@
+module CoffeeSnsHelper
+end
