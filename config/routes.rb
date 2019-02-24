5c7a97fa9cd5805265ffb33d9a818650f9f32fe7
itochukishi <kishi-h@itochu.co.jp>
2019-02-24 22:47:44 +0900

    fourth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
 resources :coffee_sns,only: [:index, :create, :show]
 resources :genre,:community,:like, only: [:index, :create, :show, :destroy, ]
 resources :user, only: [:index, :new,:create, :show,:destroy, :update]
