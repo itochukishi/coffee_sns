9a60b430704e7e9190088251c60ca393546d7bf6
itochukishi <kishi-h@itochu.co.jp>
2019-02-24 19:57:03 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-resources :coffee,only: [:index, :create, :show]
+resources :coffee_sns,only: [:index, :create, :show]
 resources :genre,:community,:like, only: [:index, :create, :show, :destroy, ]
 resources :user, only: [:index, :new,:create, :show,:destroy, :update]
 end
